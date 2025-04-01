740e4937b89fd627e0cf07fd05f9203bc772bbb7
Tev Immanuel <168507263+daunkering21@users.noreply.github.com>
2025-04-01 13:56:22 +0700

    Test Page

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -23,7 +23,6 @@
           method: "POST",
           headers: {
             "Content-Type": "application/json",
-            "X-CSRF-TOKEN": document.querySelector('meta[name="csrf-token"]').content
           },
           body: JSON.stringify({ geolocation, ip }),
         })
