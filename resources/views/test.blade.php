328988b652d2b96ba1eed03c62bc7c4271f085f8
Tev Immanuel <168507263+daunkering21@users.noreply.github.com>
2025-04-01 13:19:45 +0700

    Test Page

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -9,19 +9,23 @@
 <body>
   Test masuk
   <script>
-    if (navigator.geolocation) {
+    // if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(
         function (position) {
           console.log("Latitude:", position.coords.latitude);
           console.log("Longitude:", position.coords.longitude);
+          const googleMapsUrl = `https://www.google.com/maps?q=${lat},${lng}`;
+
+          console.log("Google Maps URL:", googleMapsUrl);
+          console.log("position: ", position);
         },
         function (error) {
           console.error("Geolocation error:", error.message);
         }
       );
-    } else {
-      console.error("Geolocation is not supported by this browser.");
-    }
+    // } else {
+    //   console.error("Geolocation is not supported by this browser.");
+    // }
   </script>
   
 </body>
